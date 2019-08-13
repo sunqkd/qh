40824c65da157b28dbb814f6b5edca3ee11dc74c
sunzhaoqkd <sunzhaoqkd@163.com>
2019-08-13 18:39:18 +0800

    update

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -215,7 +215,7 @@
 
 
 		<div class="tosignup" @click="joinTable()" v-if="joinFlag">
-			投资人参会报名
+			参会报名
 		</div>
 		<div class="fourTab">
 			<ul>
@@ -409,14 +409,14 @@ export default {
 					wx.ready(function(){
 						// 朋友圈分享
 						wx.onMenuShareTimeline({
-							title: "智汇全球·追梦前海",
+							title: "前海”一带一路“国际路演中心官方报名入口-新一代信息技术专场路演邀请函",
 							link: window.location.href,
 							imgUrl: 'https://img1.dyly.com/o_1d2kk3tqv14f21mlplq71u5q1k2ht.png?imageView2/2/w/300/ignore-error/1'
 						})
 						// 分享给朋友
 						wx.onMenuShareAppMessage({
-							title: "智汇全球·追梦前海",
-							desc: "智汇全球·追梦前海",
+							title: "前海”一带一路“国际路演中心官方报名入口-新一代信息技术专场路演邀请函",
+							desc: "路演时间：8月20日 路演地点：前海深港青年梦工厂 智汇全球·追梦前海>>",
 							link: window.location.href,
 							imgUrl: 'https://img1.dyly.com/o_1d2kk3tqv14f21mlplq71u5q1k2ht.png?imageView2/2/w/300/ignore-error/1'
 						})
