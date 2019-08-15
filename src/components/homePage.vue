faa05d9c9d22c1e84664982ef87cbf0007bbec84
sunzhaoqkd <sunzhaoqkd@163.com>
2019-08-15 11:00:53 +0800

    update

--- src/components/homePage.vue
+++ src/components/homePage.vue
@@ -341,7 +341,7 @@ export default {
 			joinFlag:false, // 参会报名显示和隐藏
 			title:'前海 “一带一路” 国际路演中心官方报名入口-新一代信息技术专场路演邀请函',
 			decs:'路演时间：8月20日 路演地点：前海深港青年梦工场 智汇全球·追梦前海>>',
-			imgUrl:'https://m.dyly.com/register/app_h5/img/logo_ehub.jpg'
+			imgUrl:'https://m.dyly.com/register/app_h5/img/logo_ehub.png'
 		}
 	},
 	created(){
